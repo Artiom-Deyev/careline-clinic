5f231c6b969e90ee4459233f15eda7c026b5f5f9
Artiom-Deyev <79644010+Artiom-Deyev@users.noreply.github.com>
2025-06-13 02:36:04 +0400

    initial commit for new repo

---
+++ logic.js
@@ -0,0 +1,22 @@
+//Nav bar to change background on scrolling
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav');
+    if (window.scrollY > 0) {
+      nav.classList.add('nav--scrolled');
+    } else {
+      nav.classList.remove('nav--scrolled');
+    }
+  });
+
+//Button on click
+const btnArr = document.querySelectorAll("#button");
+const layer = document.querySelector('.layer');
+
+btnArr.forEach(el => el.addEventListener('click', () => {layer.classList.remove("layer--none");
+}));
+
+//Layer to dissapear
+if (layer) {
+    layer.addEventListener('click', () => {layer.classList.add('layer--none');
+    })
+}
